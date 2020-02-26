cdc60ed04f64a99ed54539804a4d39b96cdb65c2
elayebMazen <el.ayeb.mazen@gmail.com>
2020-02-26 17:44:05 +0100

    add foreginKey to the conversation table

--- database/migrations/2020_02_26_143831_create_patient_table.php
+++ database/migrations/2020_02_26_143831_create_patient_table.php
@@ -14,6 +14,7 @@ class CreatePatientTable extends Migration
     public function up()
     {
         Schema::create('patient', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('email')->unique();
             $table->string('firstName');
             $table->string('lastName');

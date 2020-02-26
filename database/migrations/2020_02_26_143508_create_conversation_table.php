356c1d401a8b64405592ef70d7e58ce7b49c3b51
elayebMazen <el.ayeb.mazen@gmail.com>
2020-02-26 17:15:32 +0100

    creation tables nutritionist conversation patient

---
+++ database/migrations/2020_02_26_143508_create_conversation_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversation', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('nutritionist_id');
+            $table->foreign('nutritionist_id')->references('id')->on('nutritionist');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversation');
+    }
+}
---
+++ database/migrations/2020_02_26_143831_create_patient_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient', function (Blueprint $table) {
+            $table->string('email')->unique();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('password');
+            $table->string('gender');
+            $table->string('numberPhone');
+            $table->string('profession');
+            $table->unsignedBigInteger('nutritionist_id');
+            $table->foreign('nutritionist_id')->references('id')->on('nutritionist');
+            $table->unsignedBigInteger('conversation_id');
+            $table->foreign('conversation_id')->references('id')->on('conversation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient');
+    }
+}

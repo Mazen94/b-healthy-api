2aa64a7c0bc4a6e218a8a601c2c6b776490888cc
elayebMazen <el.ayeb.mazen@gmail.com>
2020-02-27 11:57:33 +0100

    Make models (Nutritionnist,Patient) with Relationship OneToMany

---
+++ app/Nutritionist.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Nutritionist extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+    */
+    protected $fillable = [
+        'email','password','firstName','lastName','picture'
+    ];
+
+
+    /**
+     * the has many relationship
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function patient()
+    {
+        return $this->hasMany('App\Patient');
+    }
+}
---
+++ app/Patient.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email','password','firstName','lastName','picture'
+    ];
+
+    /**
+     * One To Many (Inverse)
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function nutritionist()
+    {
+        return $this->belongsTo('App\Nutritionist');
+    }
+
+}

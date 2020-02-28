762550d6bbbbea4b0bbfb5fb316116dfb5f40b4f
elayebMazen <el.ayeb.mazen@gmail.com>
2020-02-28 09:21:12 +0100

    Make Recommandation model

---
+++ app/Recommandation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recommandation extends Model
+{
+    /* Storemenu Attributes:
+     *      int id
+     *      string aeviter
+     */
+
+    /**
+     * @var string
+     */
+    protected $table = 'recommandations';
+}

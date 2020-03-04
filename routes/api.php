b3c4c67b5c003e42fa8108c740462c9cc6308ebc
elayebMazen <el.ayeb.mazen@gmail.com>
2020-03-04 09:17:01 +0100

    add names to routes

--- routes/api.php
+++ routes/api.php
@@ -21,38 +21,69 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 
 */
-Route::prefix('nutritionist')->group(function () {
-    Route::post('register','ApiNutritionnist\AuthNutrtionnistConrtoller@register');
-    Route::post('login','ApiNutritionnist\AuthNutrtionnistConrtoller@login');
-    Route::middleware('auth:api')->group(function(){
-
-        Route::post('patients/','ApiNutritionnist\PatientController@store');
-        Route::delete('patient/{id}','ApiNutritionnist\PatientController@destroy');
-        Route::get('patient/{id}','ApiNutritionnist\PatientController@show');
-        Route::get('patients/','ApiNutritionnist\PatientController@index');
-
-        Route::post('ingredients/','ApiNutritionnist\IngredientConrtoller@store');
-        Route::delete('ingredient/{id}','ApiNutritionnist\IngredientConrtoller@destroy');
-        Route::get('ingredient/{id}','ApiNutritionnist\IngredientConrtoller@show');
-        Route::get('ingredients/','ApiNutritionnist\IngredientConrtoller@index');
-        Route::put('ingredient/{id}','ApiNutritionnist\IngredientConrtoller@update');
+Route::prefix('nutritionist')->group(
+    function () {
+        Route::post('register', 'ApiNutritionnist\AuthNutrtionnistConrtoller@register')->name('Register Nutritionnist');
+        Route::post('login', 'ApiNutritionnist\AuthNutrtionnistConrtoller@login')->name('Login Nutritionnist');
+        Route::middleware('auth:api')->group(
+            function () {
+                Route::post('patients/', 'ApiNutritionnist\PatientController@store')->name('Store Patient');
+                Route::delete('patient/{id}', 'ApiNutritionnist\PatientController@destroy')->name('Delete Patient');
+                Route::get('patient/{id}', 'ApiNutritionnist\PatientController@show')->name(
+                    'Recovery only one Patient'
+                );
+                Route::get('patients/', 'ApiNutritionnist\PatientController@index')->name('Recovery all patients');
 
 
-        Route::get('patient/{id_patient}/visits','ApiNutritionnist\VisitController@index');
-        Route::post('patient/{id_patient}/visits','ApiNutritionnist\VisitController@store');
-        Route::get('patient/{id_patient}/visit/{id_visit}','ApiNutritionnist\VisitController@show');
-        Route::put('patient/{id_patient}/visit/{id_visit}','ApiNutritionnist\VisitController@update');
-        Route::delete('patient/{id_patient}/visit/{id_visit}','ApiNutritionnist\VisitController@destroy');
+                Route::post('ingredients/', 'ApiNutritionnist\IngredientConrtoller@store')->name('Store ingredient');
+                Route::delete('ingredient/{id}', 'ApiNutritionnist\IngredientConrtoller@destroy')->name(
+                    'Delete ingredient'
+                );
+                Route::get('ingredient/{id}', 'ApiNutritionnist\IngredientConrtoller@show')->name(
+                    'Recovery only one ingredient'
+                );
+                Route::get('ingredients/', 'ApiNutritionnist\IngredientConrtoller@index')->name('Recovery ingredients');
+                Route::put('ingredient/{id}', 'ApiNutritionnist\IngredientConrtoller@update')->name(
+                    'Update ingredient'
+                );
 
-        Route::get('storemenus/','ApiNutritionnist\StoreMenuController@index');
-        Route::post('storemenus','ApiNutritionnist\StoreMenuController@store');
-        Route::get('storemenu/{id_storemenus}','ApiNutritionnist\StoreMenuController@show');
-        Route::put('storemenu/{id_storemenus}','ApiNutritionnist\StoreMenuController@update');
-        Route::delete('storemenu/{id_storemenus}','ApiNutritionnist\StoreMenuController@destroy');
 
-    });
+                Route::get('patient/{id_patient}/visits', 'ApiNutritionnist\VisitController@index')->name(
+                    'Recovery visits related to patient'
+                );
+                Route::post('patient/{id_patient}/visits', 'ApiNutritionnist\VisitController@store')->name(
+                    'Store visit related to patient'
+                );
+                Route::get('patient/{id_patient}/visit/{id_visit}', 'ApiNutritionnist\VisitController@show')->name(
+                    'Recovery only one visit related to patient'
+                );
+                Route::put('patient/{id_patient}/visit/{id_visit}', 'ApiNutritionnist\VisitController@update')->name(
+                    'Recovery visits related to patient'
+                );
+                Route::delete(
+                    'patient/{id_patient}/visit/{id_visit}',
+                    'ApiNutritionnist\VisitController@destroy'
+                )->name('Update visit related to patient');
 
-});
+                Route::get('storemenus/', 'ApiNutritionnist\StoreMenuController@index')->name('Recovery StoreMenus');
+                Route::post('storemenus', 'ApiNutritionnist\StoreMenuController@store')->name('Store a StoreMenu');
+                Route::get('storemenu/{id_storemenus}', 'ApiNutritionnist\StoreMenuController@show')->name(
+                    'Recovery only one Storemenu'
+                );
+                Route::put('storemenu/{id_storemenus}', 'ApiNutritionnist\StoreMenuController@update')->name(
+                    'update only one Storemenu'
+                );
+                Route::delete('storemenu/{id_storemenus}', 'ApiNutritionnist\StoreMenuController@destroy')->name(
+                    'delete  Storemenu'
+                );
+
+                Route::put('storemenu/{id_storemenus}/ingredient/{id_ingredient}', 'ApiNutritionnist\StoreMenuController@update')->name(
+                    'update only one Storemenu'
+                );
+            }
+        );
+    }
+);
 
 /*
 |--------------------------------------------------------------------------
@@ -60,6 +91,7 @@ Route::prefix('nutritionist')->group(function () {
 |--------------------------------------------------------------------------
 
 */
-Route::prefix('patient')->group(function () {
-
-});
+Route::prefix('patient')->group(
+    function () {
+    }
+);

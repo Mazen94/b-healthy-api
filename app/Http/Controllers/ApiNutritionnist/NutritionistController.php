4d8cd1d1bc20ac4c3bfb97eebf3e6654c952938b
elayebMazen <el.ayeb.mazen@gmail.com>
2020-03-10 12:34:48 +0100

    add code Nutritionnist controller

--- app/Http/Controllers/ApiNutritionnist/NutritionistController.php
+++ app/Http/Controllers/ApiNutritionnist/NutritionistController.php
@@ -1,10 +1,126 @@
 <?php
 
-
 namespace App\Http\Controllers\ApiNutritionnist;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterNutritionist;
+use App\Repositories\NutritionnistRepository;
+use Illuminate\Http\JsonResponse;
+use JWTAuth;
 
-class NutritionistController
+class NutritionistController extends Controller
 {
 
+    /**
+     * Display the specified resource.
+     *
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        return response()->json(
+            [
+                'success' => true,
+                'nutritionist' => auth()->user(),
+            ],
+            200
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return JsonResponse
+     */
+    public function update(RegisterNutritionist $request)
+    {
+        $nutritionist = NutritionnistRepository::updateNutritionist(
+            $request->input('email'),
+            $request->input('firstName'),
+            $request->input('lastName'),
+            $request->input('password'),
+            $request->input('picture')
+        );
+        return response()->json(
+            [
+                'success' => true,
+                'nutritionist' => $nutritionist,
+            ],
+            200
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function destroy()
+    {
+        NutritionnistRepository::deleteNutritionist();
+        return response()->json(
+            [
+                'success' => true,
+            ],
+            200
+        );
+    }
+
+    /**
+     * Nutritionist Register
+     * @param RegisterNutritionist $request
+     * @return JsonResponse
+     */
+    public function register(RegisterNutritionist $request)
+    {
+        $nutritionist = NutritionnistRepository::register(
+            $request['email'],
+            $request['firstName'],
+            $request['lastName'],
+            $request['password']
+        );
+        $token = JWTAuth::fromUser($nutritionist);
+
+
+        return response()->json(
+            [
+                'success' => true,
+                'token' => $token,
+            ],
+            200
+        );
+    }
+
+    /**
+     * Login Nutritionist
+     * @param LoginRequest $request
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'invalid_email_or_password',
+                ],
+                401
+            );
+        }
+
+
+        return response()->json(
+            [
+                'success' => true,
+                'token' => $token,
+            ],
+            200
+        );
+    }
 }

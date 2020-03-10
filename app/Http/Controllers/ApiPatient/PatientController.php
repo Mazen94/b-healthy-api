331cec60305f304f67af4402c44f0f7738f2b791
elayebMazen <el.ayeb.mazen@gmail.com>
2020-03-10 17:38:55 +0100

    Delete AuthPatientController and change the login to PatientController

--- app/Http/Controllers/ApiPatient/PatientController.php
+++ app/Http/Controllers/ApiPatient/PatientController.php
@@ -3,24 +3,62 @@
 namespace App\Http\Controllers\ApiPatient;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\PatientRequest;
 use App\Http\Requests\RegisterPatient;
 use App\Repositories\PatientRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use JWTAuth;
 
 class PatientController extends Controller
 {
+    /**
+     * Login Patient
+     * @param LoginRequest $request
+     *
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $token = null;
+
+        if (!$token = auth('api-patient')->attempt($credentials)) {
+            return response()->json(
+                [
+                    'response' => 'error',
+                    'message' => 'invalid_email_or_password',
+                ]
+            );
+        }
+
+
+        return response()->json(
+            [
+                'response' => 'success',
+
+                'token' => $token,
+
+            ]
+        );
+    }
 
     /**
-     * Display a listing of the resource.
+     * Display the specified resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        return JWTAuth::parseToken()->authenticate();
+        return response()->json(
+            [
+                'success' => true,
+                'patient' => auth()->user(),
+            ],
+            200
+        );
     }
 
 

d2a9efdb4979e75b29109bd8af6254cb3e8071e3
elayebMazen <el.ayeb.mazen@gmail.com>
2020-03-10 13:35:07 +0100

    Change RegisterPatient to PatientRequest

---
+++ app/Http/Requests/PatientRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Config;
+
+class PatientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ($this->route()->getName() == 'registerPatient') {
+            return [
+                'email' => 'required|string|email|unique:patients',
+                'firstName' => 'required',
+                'lastName' => 'required',
+                'password' => 'required|min:' . Config::get('constants.MIN_PASSWORD_LENGTH') . '|max:' . Config::get(
+                        'constants.MAX_PASSWORD_LENGTH'
+                    ),
+                'numberPhone' => 'required|string',
+                'profession' => 'required|string',
+                'gender' => 'required|string',
+            ];
+        }
+        else {
+            $user = auth()->user();
+            return [
+                'email' => [
+                    'required',
+                    Rule::unique('patients', 'email')->ignore($user->id)
+                ],
+                'firstName' => 'required',
+                'lastName' => 'required',
+                'password' => 'min:' . Config::get('constants.MIN_PASSWORD_LENGTH') . '|max:' . Config::get(
+                        'constants.MAX_PASSWORD_LENGTH'
+                    ),
+                'numberPhone' => 'required|string',
+                'profession' => 'required|string',
+                'gender' => 'required|string',
+            ];
+        }
+    }
+}

68eb087be006ea0eaec3989f22c6c4e2732b421e
elayebMazen <el.ayeb.mazen@gmail.com>
2020-03-12 11:36:15 +0100

    camelCase id

--- routes/mobile/mobile.php
+++ routes/mobile/mobile.php
@@ -2,37 +2,30 @@
 
 Route::prefix('patient')->group(
     function () {
-        Route::post('login', 'ApiPatient\PatientController@login');
-        Route::middleware('auth:api-patient')->group(
-            function () {
+        Route::post('login', 'ApiPatient\AuthController@login');
+        Route::middleware('auth:api-patient')->group(function () {
                 Route::get('/', 'ApiPatient\PatientController@index');
                 Route::put('/', 'ApiPatient\PatientController@update')->name('updatePatient');
 
-                Route::prefix('recommendation')->group(
-                    function () {
+                Route::prefix('recommendation')->group(function () {
                         Route::get('/', 'ApiPatient\RecommendationController@index');
                         Route::get('/menus', 'ApiPatient\RecommendationController@indexMenus');
-                        Route::post('/{id_recommendation}/menus', 'ApiPatient\MenuController@store');
-                    }
-                );
+                        Route::post('/{idRecommendation}/menus', 'ApiPatient\MenuController@store');
+                });
 
 
-                Route::prefix('notifications')->group(
-                    function () {
+                Route::prefix('notifications')->group(function () {
                         Route::get('/', 'ApiPatient\NotificationController@index');
                         Route::post('/', 'ApiPatient\NotificationController@store');
-                        Route::delete('/{id_notification}', 'ApiPatient\NotificationController@destroy');
-                        Route::get('/{id_notification}', 'ApiPatient\NotificationController@show');
-                    }
-                );
+                        Route::delete('/{idNotification}', 'ApiPatient\NotificationController@destroy');
+                        Route::get('/{idNotification}', 'ApiPatient\NotificationController@show');
+                });
 
-                Route::prefix('activity')->group(
-                    function () {
+                Route::prefix('activity')->group(function () {
                         Route::get('/', 'ApiPatient\PhysicalActiviteController@index');
                         Route::post('/', 'ApiPatient\PhysicalActiviteController@store');
 
-                    }
-                );
+                });
             }
         );
     }

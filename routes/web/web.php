328b5910bc49016083b24844d7bad08ea21ddd94
elayebMazen <el.ayeb.mazen@gmail.com>
2020-03-12 09:33:17 +0100

    Change the attributs to camelCase

--- routes/web/web.php
+++ routes/web/web.php
@@ -10,103 +10,84 @@ Route::prefix('nutritionist')->group(
     function () {
         Route::post('register', 'ApiNutritionnist\NutritionistController@register')->name('registerNutritionist');
         Route::post('login', 'ApiNutritionnist\NutritionistController@login');
-        Route::middleware('auth:api')->group(
-            function () {
+        Route::middleware('auth:api')->group(function () {
                 Route::get('/', 'ApiNutritionnist\NutritionistController@index');
                 Route::put('/', 'ApiNutritionnist\NutritionistController@update')->name('updateNutritionist');
 
-                Route::prefix('patients')->group(
-                    function () {
+                Route::prefix('patients')->group(function () {
                         Route::post('/', 'ApiNutritionnist\PatientController@store')->name('registerPatient');
                         Route::get('/', 'ApiNutritionnist\PatientController@index');
 
 
-                        Route::prefix('{id_patient}')->group(
-                            function () {
+                        Route::prefix('{idPatient}')->group(function () {
                                 Route::delete('/', 'ApiNutritionnist\PatientController@destroy');
                                 Route::get('/', 'ApiNutritionnist\PatientController@show');
 
 
-                                Route::prefix('visits')->group(
-                                    function () {
+                                Route::prefix('visits')->group(function () {
                                         Route::get('/', 'ApiNutritionnist\VisitController@index');
                                         Route::post('/', 'ApiNutritionnist\VisitController@store');
-                                        Route::get('/{id_visit}', 'ApiNutritionnist\VisitController@show');
-                                        Route::put('/{id_visit}', 'ApiNutritionnist\VisitController@update');
-                                        Route::delete('/{id_visit}', 'ApiNutritionnist\VisitController@destroy');
-                                    }
-                                );
+                                        Route::get('/{idVisit}', 'ApiNutritionnist\VisitController@show');
+                                        Route::put('/{idVisit}', 'ApiNutritionnist\VisitController@update');
+                                        Route::delete('/{idVisit}', 'ApiNutritionnist\VisitController@destroy');
+                                    });
 
 
-                                Route::prefix('recommendations')->group(
-                                    function () {
+                                Route::prefix('recommendations')->group(function () {
                                         Route::get('/', 'ApiNutritionnist\RecommendationController@index');
                                         Route::post('/', 'ApiNutritionnist\RecommendationController@store');
                                         Route::get(
-                                            '/{id_recommendation}',
+                                            '/{idRecommendation}',
                                             'ApiNutritionnist\RecommendationController@show'
                                         );
                                         Route::put(
-                                            '/{id_recommendation}',
+                                            '/{idRecommendation}',
                                             'ApiNutritionnist\RecommendationController@update'
                                         );
                                         Route::delete(
-                                            '/{id_recommendation}',
+                                            '/{idRecommendation}',
                                             'ApiNutritionnist\RecommendationController@destroy'
                                         );
-                                        Route::prefix('{id_recommendation}/menus')->group(
-                                            function () {
+                                        Route::prefix('{idRecommendation}/menus')->group(function () {
                                                 Route::post(
                                                     '/',
                                                     'ApiNutritionnist\RecommendationController@addMenuToRecommendation'
                                                 );
                                                 Route::delete(
-                                                    '/{id_menu}',
+                                                    '/{idMenu}',
                                                     'ApiNutritionnist\RecommendationController@destroyMenu'
                                                 );
-                                            }
-                                        );
-                                    }
-                                );
-                            }
-                        );
-                    }
-                );
+                                        });
+                                });
+                        });
+                });
 
 
-                Route::prefix('ingredients')->group(
-                    function () {
+                Route::prefix('ingredients')->group(function () {
                         Route::post('/', 'ApiNutritionnist\IngredientConrtoller@store');
                         Route::delete('/{id}', 'ApiNutritionnist\IngredientConrtoller@destroy');
                         Route::get('/{id}', 'ApiNutritionnist\IngredientConrtoller@show');
                         Route::get('/', 'ApiNutritionnist\IngredientConrtoller@index');
                         Route::put('/{id}', 'ApiNutritionnist\IngredientConrtoller@update');
-                    }
-                );
+                });
 
 
-                Route::prefix('storemenus')->group(
-                    function () {
+                Route::prefix('storemenus')->group(function () {
                         Route::get('/', 'ApiNutritionnist\StoreMenuController@index');
                         Route::get('/ages', 'ApiNutritionnist\StoreMenuController@showByAge');
                         Route::post('/', 'ApiNutritionnist\StoreMenuController@store');
-                        Route::get('/{id_storemenus}', 'ApiNutritionnist\StoreMenuController@show');
-                        Route::put('/{id_storemenus}', 'ApiNutritionnist\StoreMenuController@update');
-                        Route::delete('/{id_storemenus}', 'ApiNutritionnist\StoreMenuController@destroy');
-                        Route::prefix('{id_storemenus}/ingredients')->group(
-                            function () {
+                        Route::get('/{idStoreMenu}', 'ApiNutritionnist\StoreMenuController@show');
+                        Route::put('/{idStoreMenu}', 'ApiNutritionnist\StoreMenuController@update');
+                        Route::delete('/{idStoreMenu}', 'ApiNutritionnist\StoreMenuController@destroy');
+                        Route::prefix('{idStoreMenu}/ingredients')->group(function () {
                                 Route::post('/', 'ApiNutritionnist\StoreMenuController@addIngredient');
                                 Route::delete(
-                                    '{id_ingredient}',
+                                    '{idIngredient}',
                                     'ApiNutritionnist\StoreMenuController@deleteIngredient'
                                 );
-                                Route::put('{id_ingredient}', 'ApiNutritionnist\StoreMenuController@updateAmountPivotIngredient');
-                            }
-                        );
-                    }
-                );
-            }
-
-        );
+                                Route::put('{idIngredient}', 'ApiNutritionnist\StoreMenuController@updateAmountPivotIngredient');
+                        });
+                });
+        });
     }
 );

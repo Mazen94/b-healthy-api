cea01c130816312a7de89ad08a6f8affb0f97e9c
elayebMazen <el.ayeb.mazen@gmail.com>
2020-03-13 15:45:57 +0100

    create new model recommendation

---
+++ app/Models/Recommendation.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * Class Recommendation
+ * @package App
+ *
+ * @property int $id
+ * @property string $avoid
+ */
+class Recommendation extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'recommendations';
+
+    /**
+     * Many To Many RelationShip
+     * @return BelongsToMany
+     */
+    public function patients()
+    {
+        return $this->belongsToMany('App\Recommendation');
+    }
+
+    /**
+     * Many To Many RelationShip between menu et recommandetion
+     * @return BelongsToMany
+     */
+    public function menus()
+    {
+        return $this->belongsToMany(
+            'App\Menu',
+            'menus_recommandations',
+            'recommandation_id',
+            'menu_id'
+        )->using('App\RecommandationMenu');
+    }
+}

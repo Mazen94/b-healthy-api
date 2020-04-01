d9fea76d33680239c4bbd0e18eb6f3d00f7ed652
elayebMazen <el.ayeb.mazen@gmail.com>
2020-04-01 09:43:23 +0200

    controller for statistical APIs

---
+++ app/Http/Controllers/ApiNutritionist/StatisticalController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ApiNutritionist;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StatisticalController extends Controller
+{
+    //
+}

57e5f4c20541a573e6d88318668cbb3652a10a0d
elayebMazen <el.ayeb.mazen@gmail.com>
2020-04-12 10:54:45 +0200

    Array of age

--- config/constants.php
+++ config/constants.php
@@ -32,5 +32,7 @@ return [
     //minimum calories of a store menu
     'MIN_CALORIES_LENGTH' => 100,
 
+    'ARRAY_OF_AGE' => [[10,15],[16,20],[21,25],[26,30],[31,35],[36,40],[41,45],[46,50],[51,55],[56,60]]
+
 
 ];

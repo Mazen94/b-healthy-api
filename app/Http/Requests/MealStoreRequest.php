b93ab0878066202aa5f3fe5ea6f0553a259d90a2
elayebMazen <el.ayeb.mazen@gmail.com>
2020-05-04 15:24:48 +0200

    remove the rule

--- app/Http/Requests/MealStoreRequest.php
+++ app/Http/Requests/MealStoreRequest.php
@@ -27,7 +27,7 @@ class MealStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'type_menu' => Rule::in(MealStore::TYPE_MENU) . 'required|integer',
+            'type_menu' =>  'required|integer',
             'name' => 'required|string',
             'max_age' => 'required|integer|between:' . Config::get('constants.MIN_AGE_LENGTH') . ',' . Config::get(
                     'constants.MAX_AGE_LENGTH'

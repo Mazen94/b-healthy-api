7c67736f9dc1448c708ac087078de4ad49fa0dc0
elayebMazen <el.ayeb.mazen@gmail.com>
2020-06-03 16:03:22 +0200

    create folder fr for the french messages

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'newRecommendation' => 'Une nouvelle recommandations pour vous',
+    'RecommendationUpdated' => 'Votre recommendation a été modifier',
+    'recommendationHeading' => ["en" => "Recommandation"]
+];

3956e06699dd14caad6d9977daac4f378af35c7d
elayebMazen <el.ayeb.mazen@gmail.com>
2020-06-04 15:40:54 +0200

    add new string

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -3,5 +3,6 @@
 return [
     'newRecommendation' => 'Une nouvelle recommandations pour vous',
     'RecommendationUpdated' => 'Votre recommendation a été modifier',
-    'recommendationHeading' => ["en" => "Recommandation"]
+    'recommendationHeading' => ["en" => "Recommandation"],
+     'emailFailed' => 'Ton adresse email n\'est pas valide.'
 ];
